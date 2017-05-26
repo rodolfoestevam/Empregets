296ffb40ea84358466db96924671baa5fbcae255
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2017-05-26 16:27:39 -0300

    [Especialidade] Adicionado DAO, embora está faltando as validações.

--- Biblioteca/especialidade/EspecialidadeBD.cs
+++ Biblioteca/especialidade/EspecialidadeBD.cs
@@ -1,12 +1,186 @@
-﻿using System;
+﻿using Biblioteca.Utils;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Biblioteca.especialidade
 {
-    class EspecialidadeBD
+    public class EspecialidadeBD : ConexaoSQL, IEspecialidade
     {
+        public void CadastrarEspecialidade(Especialidade especialidade)
+        {
+            try
+            {
+                //abrir conexão
+                this.abrirConexao();
+                string sql = "INSERT INTO Especialidade (Descricao) VALUES(@Descricao)";
+               
+                SqlCommand cmd = new SqlCommand(sql, this.sqlConn);
+                
+                cmd.Parameters.Add("@Descricao", SqlDbType.VarChar);
+                cmd.Parameters["@Descricao"].Value = especialidade.Descricao;
+
+                //executando a instrução
+                cmd.ExecuteNonQuery();
+                //liberando memória
+                cmd.Dispose();
+                //fechando conexão
+                this.fecharConexao();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro ao conectar e cadastrar " + err.Message);
+            }
+        }
+
+        public void AtualizarEspecialidade(Especialidade especialidade)
+        {
+            try
+            {
+                //abrir conexão
+                this.abrirConexao();
+                string sql = "UPDATE Especialidade SET Descricao = @Descricao WHERE ID_Especialidade = @ID_Especialidade";
+                //instrução a ser executada
+                SqlCommand cmd = new SqlCommand(sql, this.sqlConn);
+
+                cmd.Parameters.Add("@ID_Especialidade", SqlDbType.Int);
+                cmd.Parameters["@ID_Especialidade"].Value = especialidade.ID_Especialidade;
+
+                cmd.Parameters.Add("@Descricao", SqlDbType.VarChar);
+                cmd.Parameters["@Descricao"].Value = especialidade.Descricao;
+
+                //executando a instrução
+                cmd.ExecuteNonQuery();
+                //liberando memória
+                cmd.Dispose();
+                //fechando conexão
+                this.fecharConexao();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro ao conectar e atualizar " + err.Message);
+            }
+        }
+
+        public void RemoverEspecialidade(Especialidade especialidade)
+        {
+            try
+            {
+                //abrir conexão
+                this.abrirConexao();
+                string sql = "DELETE FROM Especialidade WHERE ID_Especialidade = @Id_Especialidade";
+                //instrução a ser executada
+                SqlCommand cmd = new SqlCommand(sql, this.sqlConn);
+
+                cmd.Parameters.Add("@Id_Especialidade", SqlDbType.Int);
+                cmd.Parameters["@Id_Especialidade"].Value = especialidade.ID_Especialidade;
+
+                //executando
+                cmd.ExecuteNonQuery();
+                //liberando memória
+                cmd.Dispose();
+                //fechando conexão
+                this.fecharConexao();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro ao conectar e remover " + err.Message);
+            }
+        }
+
+        public List<Especialidade> ListarEspecialidade(Especialidade filtro)
+        {
+            List<Especialidade> retorno = new List<Especialidade>();
+            try
+            {
+                //abrir conexão
+                this.abrirConexao();
+                string sql = "SELECT ID_Especialidade, Descricao FROM Especialidade WHERE ID_Especialidade = ID_Especialidade";
+                //se foi passada um id válido, este id entrará como critério de filtro
+                if (filtro.ID_Especialidade > 0)
+                {
+                    sql += "AND ID_Especialidade = @ID_Especialidade";
+                }
+                //se foi passada uma descrição válida, esta descrição entrará como critério de filtro
+                if (filtro.Descricao != null && filtro.Descricao.Trim().Equals("") == false)
+                {
+                    sql += "AND Descricao LIKE '%@Descricao%'";
+                }
+                SqlCommand cmd = new SqlCommand(sql, sqlConn);
+
+                //se foi passada um id válido, este id entrará como critério de filtro
+                if (filtro.ID_Especialidade > 0)
+                {
+                    cmd.Parameters.Add("@Id_Especialidade", SqlDbType.Int);
+                    cmd.Parameters["@ID_Especialidade"].Value = filtro.Descricao;
+                }
+                //se foi passada uma descrição válida, esta descrição entrará como critério de filtro
+                if (filtro.Descricao != null && filtro.Descricao.Trim().Equals("") == false)
+                {
+                    cmd.Parameters.Add("@Descricao", SqlDbType.VarChar);
+                    cmd.Parameters["@Descricao"].Value = filtro.Descricao;
+                }
+
+                //executando a instrução e colocando o resultado em um leitor
+                SqlDataReader DbReader = cmd.ExecuteReader();
+                //lendo uma consulta
+                while (DbReader.Read())
+                {
+                    Especialidade especialidade = new Especialidade();
+                    //acessando os valores das colunas do resultado
+                    especialidade.ID_Especialidade = DbReader.GetInt32(DbReader.GetOrdinal("ID_Especialidade"));
+                    especialidade.Descricao = DbReader.GetString(DbReader.GetOrdinal("Descricao"));
+                    retorno.Add(especialidade);
+                }
+                //fechando o leitor de resultados
+                DbReader.Close();
+                //liberando memória
+                cmd.Dispose();
+                //fechando conexão
+                this.fecharConexao();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro ao conectar e listar " + err.Message);
+            }
+            return retorno;
+        }
+
+        public bool VerificarExistenciaEspecialidade(Especialidade especialidade)
+        {
+            bool retorno = false;
+
+            try
+            {
+                this.abrirConexao();
+                string sql = "SELECT ID_Especialidade, Descricao FROM Especialidade WHERE ID_Especialidade = @ID_Especialidade";
+                SqlCommand cmd = new SqlCommand(sql, sqlConn);
+                cmd.Parameters.Add("@ID_Especialidade", SqlDbType.Int);
+                cmd.Parameters["@ID_Especialidade"].Value = especialidade.ID_Especialidade;
+                //executando a instrução
+                SqlDataReader DbReader = cmd.ExecuteReader();
+                //lendo o resultado
+                while (DbReader.Read())
+                {
+                    retorno = true;
+                    break;
+                }
+                //fechando o leitor de resultados
+                DbReader.Close();
+                //liberando memória
+                cmd.Dispose();
+                //fechando conexão
+                this.fecharConexao();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro ao conectar e selecionar " + err.Message);
+            }
+            return retorno;
+        }
     }
 }
--- Biblioteca/especialidade/IEspecialidade.cs
+++ Biblioteca/especialidade/IEspecialidade.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.especialidade
 {
-    class IEspecialidade
+    public interface IEspecialidade
     {
+        //Cadastrar uma especialidade
+        void CadastrarEspecialidade(Especialidade especialidade);
+        //Atualizar uma especialidade
+        void AtualizarEspecialidade(Especialidade especialidade);
+        //Remover uma especialidade
+        void RemoverEspecialidade(Especialidade especialidade);
+        //Selecionar as especialidades que se encaixam no filtro de pesquisa
+        List<Especialidade> ListarEspecialidade(Especialidade filtro);
+        //Verificar se uma especialidade já está cadastrado
+        bool VerificarExistenciaEspecialidade(Especialidade especialidade);
     }
 }
